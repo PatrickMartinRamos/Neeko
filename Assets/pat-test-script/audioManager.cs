bfafc0e0577bd54b97464ba02e8c8028d967b775
Patrick Ramos <patrick.ramos@ciit.edu.ph>
2024-07-11 17:09:22 +0800

    add city ambience / button sfx

---
+++ Assets/pat-test-script/audioManager.cs
@@ -0,0 +1,70 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class audioManager : MonoBehaviour
+{
+    public static audioManager instance;
+
+    [System.Serializable]
+    public class Sound
+    {
+        public string name;
+        public AudioClip clip;
+        [Range(0f, 1f)]
+        public float volume = 0.7f;
+        public bool loop = false;
+        public bool playonAwake = false;
+
+        [HideInInspector]
+        public AudioSource source;
+    }
+
+    public List<Sound> sounds = new List<Sound>();
+
+    void Awake()
+    {
+        if (instance == null)
+        {
+            instance = this;
+        }
+        else
+        {
+            Destroy(gameObject);
+            return;
+        }
+
+        foreach (Sound _sounds in sounds)
+        {
+            _sounds.source = gameObject.AddComponent<AudioSource>();
+            _sounds.source.clip = _sounds.clip;
+            _sounds.source.volume = _sounds.volume;
+            _sounds.source.loop = _sounds.loop;
+            _sounds.source.playOnAwake = _sounds.playonAwake;
+        }
+    }
+    public void Play(string name)
+    {
+        // Find the Sound object in the sounds List where the name matches the provided 'name'.
+        Sound _sounds = sounds.Find(sound => sound.name == name);
+
+        if (_sounds == null)
+        {
+            Debug.LogWarning("Sound: " + name + " not found!");
+            return;
+        }
+        _sounds.source.Play();
+    }
+
+    public void Stop(string name)
+    {
+        Sound _sounds = sounds.Find(sound => sound.name == name);
+        if (_sounds == null)
+        {
+            Debug.LogWarning("Sound: " + name + " not found!");
+            return;
+        }
+
+        _sounds.source.Stop();
+    }
+}
